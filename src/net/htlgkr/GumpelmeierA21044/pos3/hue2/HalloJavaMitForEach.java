bbfc486cc4fabf234197a6c2c9078b0973dab8ad
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-24 14:15:48 +0200

    created HalloJavaMitForEach and implemented the forEach Methods

---
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/HalloJavaMitForEach.java
@@ -0,0 +1,17 @@
+package net.htlgkr.GumpelmeierA21044.pos3.hue2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavaMitForEach {
+    private List<String> stringList = new ArrayList<String>();
+
+    public void printList() {
+        stringList.add("java");
+        stringList.add("python");
+
+        for(int i = 0; i < stringList.size(); i++) {
+            System.out.println(stringList.get(i));
+        }
+    }
+}
