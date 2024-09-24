04636567e797deb37d939d7c0337c28b20f95e3b
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-24 15:39:20 +0200

    implemented showMethods to display the menues in Main class

--- src/net/htlgkr/GumpelmeierA21044/pos3/hue2/Main.java
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/Main.java
@@ -3,7 +3,41 @@ package net.htlgkr.GumpelmeierA21044.pos3.hue2;
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final AbstractCalculation calculator = null;
+
     public static void main(String[] args) {
 
+    }
+
+    private static void showCalculatorMenue () {
+        System.out.println("Choose calculator: \n" +
+                "           1 -- Relational calculator\n" +
+                "           2 -- Vector calculator\n" +
+                "           3 -- Complex calculator\n" +
+                "           4 -- Exit programm");
+    }
+
+    private static void showOperationMenue () {
+        System.out.println("Choose calculator: \n" +
+                "           1 -- add\n" +
+                "           2 -- substract\n" +
+                "           3 -- multiply\n" +
+                "           4 -- divide\n" +
+                "           5 -- enter numbers again");
+    }
+
+    private static void displayResult(Number result) {
+        System.out.println("Solution:\n" +
+                "          a = " + result.getA() + "\n" +
+                "          b = " + result.getB());
+    }
+
+    private static Number numberInput() {
+        System.out.print("Enter number ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter number ");
+        double b = scanner.nextDouble();
+        return new Number(a, b);
     }
 }
--- src/net/htlgkr/GumpelmeierA21044/pos3/hue2/Number.java
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/Number.java
@@ -4,7 +4,7 @@ public class Number{
     private double a;
     private double b;
 
-    public Number(double a, int b) {
+    public Number(double a, double b) {
         this.a = a;
         this.b = b;
     }
