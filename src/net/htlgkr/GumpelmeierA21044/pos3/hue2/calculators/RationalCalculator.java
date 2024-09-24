efaf1e738ba68716ab3c6479dad944af5edf7218
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-24 14:45:02 +0200

    created Calculator classes and implemented Methods from AbstractCalculation

---
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/calculators/RationalCalculator.java
@@ -0,0 +1,32 @@
+package net.htlgkr.GumpelmeierA21044.pos3.hue2.calculators;
+
+import net.htlgkr.GumpelmeierA21044.pos3.hue2.AbstractCalculation;
+import net.htlgkr.GumpelmeierA21044.pos3.hue2.Number;
+import net.htlgkr.GumpelmeierA21044.pos3.hue2.interfaces.CalculationOperation;
+
+public class RationalCalculator extends AbstractCalculation {
+
+    public RationalCalculator(CalculationOperation add, CalculationOperation substract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, substract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number substract(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return null;
+    }
+}
