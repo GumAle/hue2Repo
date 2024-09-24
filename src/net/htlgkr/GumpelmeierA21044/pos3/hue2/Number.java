28e93b559fb6abf10e508e3dc4173e9d7157f313
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-24 14:33:43 +0200

    implemented getter and setter Methods in class Number

--- src/net/htlgkr/GumpelmeierA21044/pos3/hue2/Number.java
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/Number.java
@@ -1,4 +1,22 @@
 package net.htlgkr.GumpelmeierA21044.pos3.hue2;
 
-public class Number g{
+public class Number{
+    private double a;
+    private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
 }
