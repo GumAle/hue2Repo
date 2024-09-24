55fdd24b5d1e96c90abff284d4a7e54b1f2e8b1a
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-24 14:39:15 +0200

    implemented missing Methods in abstract class

--- src/net/htlgkr/GumpelmeierA21044/pos3/hue2/AbstractCalculation.java
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/AbstractCalculation.java
@@ -14,4 +14,9 @@ public abstract class AbstractCalculation {
         this.multiply = multiply;
         this.divide = divide;
     }
+
+    public abstract Number add(Number a, Number b);
+    public abstract Number substract(Number a, Number b);
+    public abstract Number multiply(Number a, Number b);
+    public abstract Number divide(Number a, Number b);
 }
