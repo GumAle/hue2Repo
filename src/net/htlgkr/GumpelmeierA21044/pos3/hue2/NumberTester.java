ceb2e897bcce984c513a12b3af2229bc9260480b
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-24 14:19:36 +0200

    created interface NumberTest

--- src/net/htlgkr/GumpelmeierA21044/pos3/hue2/NumberTester.java
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/NumberTester.java
@@ -2,4 +2,11 @@ package net.htlgkr.GumpelmeierA21044.pos3.hue2;
 
 public class NumberTester {
 
+    public NumberTester(String filename) {
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
 }
