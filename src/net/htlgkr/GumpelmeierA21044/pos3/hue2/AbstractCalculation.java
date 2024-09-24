ab9c7e8773ada86ac91a79389a22dc1986ed4149
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-24 14:38:16 +0200

    implemented constructor in abstract class

--- src/net/htlgkr/GumpelmeierA21044/pos3/hue2/AbstractCalculation.java
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue2/AbstractCalculation.java
@@ -1,5 +1,17 @@
 package net.htlgkr.GumpelmeierA21044.pos3.hue2;
 
-public abstract class AbstractCalculation {
+import net.htlgkr.GumpelmeierA21044.pos3.hue2.interfaces.CalculationOperation;
 
+public abstract class AbstractCalculation {
+    protected CalculationOperation add;
+    protected CalculationOperation substract;
+    protected CalculationOperation multiply;
+    protected CalculationOperation divide;
+
+    public AbstractCalculation(CalculationOperation add, CalculationOperation substract, CalculationOperation multiply, CalculationOperation divide) {
+        this.add = add;
+        this.substract = substract;
+        this.multiply = multiply;
+        this.divide = divide;
+    }
 }
